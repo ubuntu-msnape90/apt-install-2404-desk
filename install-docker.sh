bff847583f243cc5df4cf1f1adab525292db710a
msnape90 <118859688+msnape90@users.noreply.github.com>
2024-12-08 02:59:42 -0500

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-sudo apt update && curl -fsSL https://get.docker.com -o get-docker.sh
+sudo apt update && \
+curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
